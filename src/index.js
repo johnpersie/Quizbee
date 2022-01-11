cb03353203eae1bb6cbb5982a5472711aa30bd11
johnpersie <83553199+johnpersie@users.noreply.github.com>
2022-01-11 13:57:54 +0100

    create the QuestionBox component which carries two props (questions and options with array of answer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
 class QuizBee extends Component {
   state = {
@@ -23,7 +24,13 @@ class QuizBee extends Component {
         <div className="title">QuizBee</div>
         {this.state.questionBank.length > 0 &&
           this.state.questionBank.map(
-            ({ question, answers, correct, questionId }) => <h4>{question}</h4>
+            ({ question, answers, correct, questionId }) => (
+              <QuestionBox
+                question={question}
+                Option={answers}
+                key={questionId}
+              />
+            )
           )}
       </div>
     );
