8f83a7d74a638e2885d458223c0d225146a4a6fc
johnpersie <83553199+johnpersie@users.noreply.github.com>
2022-01-12 15:16:40 +0100

    compute the users response and maintain a score using a props called 'selected'and pass the user response from the questionBox component to the quizbee component

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@ import QuestionBox from "./components/QuestionBox";
 class QuizBee extends Component {
   state = {
     questionBank: [],
+    score: 0,
+    responses: 0,
   };
   getQuestions = () => {
     quizService().then((question) => {
       this.setState({
         questionBank: question,
       });
+    });
+  };
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      this.setState({
+        score: this.state.score + 1,
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5,
     });
   };
   componentDidMount() {
@@ -23,12 +35,14 @@ class QuizBee extends Component {
       <div className="container">
         <div className="title">QuizBee</div>
         {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
           this.state.questionBank.map(
             ({ question, answers, correct, questionId }) => (
               <QuestionBox
                 question={question}
                 options={answers}
                 key={questionId}
+                selected={(answer) => this.computeAnswer(answer, correct)}
               />
             )
           )}
