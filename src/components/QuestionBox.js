95240b90c84240c9f8c1edb2b52e6713630998ef
johnpersie <83553199+johnpersie@users.noreply.github.com>
2022-01-15 13:44:21 +0100

    fix bug on the answer button

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 const QuestionBox = ({ question, options, selected }) => {
   const [answer, setAnswer] = useState(options);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleClick = () => {
+    setIsDisabled(!isDisabled);
+  };
+
   return (
     <div className="questionBox">
       <div className="question">{question}</div>
       {answer.map((text, index) => (
         <button
+          disabled={isDisabled}
           key={index}
           className="answerBtn"
           onClick={() => {
             setAnswer([text]);
             selected(text);
+            setIsDisabled(handleClick);
           }}
         >
           {text}
